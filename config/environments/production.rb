e1bbf5c372f0cbcd0dc76a21c5f8dd9287e35fcf
etiennepuig <wvetienne@gmail.com>
2017-07-14 23:21:25 +0200

    oublie de domaine

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yourapp.heroku.com',
+    :domain         => 'olvodev.heroku.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
